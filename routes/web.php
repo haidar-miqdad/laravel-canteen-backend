00a14b7fd4def9e0f3aea5a396c0f9747bcea1db
haidar-miqdad <haidarmiqdad2004@gmail.com>
2024-10-18 11:04:06 +0700

    feat: login authentication

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::get('home', function () {
+    return view('pages.dashboard');
+});
+
+//routing otentikasi dipindahin ke app/providers/fortifyServiceprovider
+// Route::get('/login', function () {
+//     return view('pages.auth.login');
+// })->name('login');
+
+// Route::get('/register', function () {
+//     return view('pages.auth.register');
+// })->name('register');
